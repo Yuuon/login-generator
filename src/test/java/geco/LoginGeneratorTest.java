e24d3abee83295a9ee9f84b8a9dd8609f1df8a50
Yuuon <yuuon.himemiya@gmail.com>
2016-11-22 10:00:36 +0100

    login-generator-2 #2 → Couvrir LoginGenerator à 100 % par des tests unitaires coder un seul de ces tests

---
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -0,0 +1,21 @@
+package geco;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 2016/11/22.
+ */
+public class LoginGeneratorTest {
+    LoginService loginService = new LoginService(new String[] {"JROL", "BPER",
+            "CGUR", "JDUP", "JRAL", "JRAL1"});
+    LoginGenerator loginGenerator = new LoginGenerator(loginService);
+
+    @Test
+    public void generateLoginForNomAndPrenom() throws Exception {
+        String gene1 = loginGenerator.generateLoginForNomAndPrenom("Paul","Durand");
+        assertEquals(gene1,"PDUR");
+    }
+
+}
